070de3bb619129cdedc95dd5273c86a0b4e0773e
Yunus Emre Aydın <47641431+YunusEmre0037@users.noreply.github.com>
2022-08-06 20:00:12 +0300

    Upload typing_test.py

---
+++ typing_test.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+from userdata import username, password
+
+# Seconds for program to wait until finding web elements
+search_time = 10
+
+class Application:
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.login()
+        self.playTypingTest()
+        self.saveScore()
+
+    def login(self):
+        self.driver.get('https://humanbenchmark.com/login')
+        username_input = WebDriverWait(self.driver, search_time).until(EC.presence_of_element_located((By.NAME, 'username')))
+        password_input = WebDriverWait(self.driver, search_time).until(EC.presence_of_element_located((By.NAME, 'password')))
+
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+
+        self.driver.find_element_by_xpath('/html/body/div[1]/div/div[4]/div/div/form/p[3]/input').click()
+        sleep(1)
+
+    def playTypingTest(self):
+        self.driver.get("https://humanbenchmark.com/tests/typing")
+        letters = WebDriverWait(self.driver, search_time).until(EC.presence_of_element_located((By.CLASS_NAME, 'letters')))
+        text = letters.text
+        letters.send_keys(text)
+
+    def saveScore(self):
+        WebDriverWait(self.driver, search_time).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Save score')]"))).click()
+        print("Press enter to close the program...", end='')
+        input()
+        self.driver.close()
+
+
+def main():
+    app = Application()
+
+if __name__ == '__main__':
+    main()
