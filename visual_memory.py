a17f905765cd127e3acde57a7acbee7c3806cf63
DolphyWind <yunusaydin590@gmail.com>
2022-08-27 19:01:40 +0300

    Enable saving score in visual_memory.py (Previously disabled for testing purposes)

--- visual_memory.py
+++ visual_memory.py
@@ -94,7 +94,7 @@ class Application:
             rounds += 1
 
     def saveScore(self):
-        #WebDriverWait(self.driver, search_time).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Save score')]"))).click()
+        WebDriverWait(self.driver, search_time).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Save score')]"))).click()
         print("Press enter to close the program...", end='')
         input()
         self.driver.close()
